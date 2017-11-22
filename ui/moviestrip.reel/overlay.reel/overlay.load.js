3bcfe43bcf44e1760ca9f93c53847acaa9c4a5f1
Harold Thetiot <hthetiot@gmail.com>
2017-11-22 13:11:21 -0800

    update dist

--- ui/moviestrip.reel/overlay.reel/overlay.load.js
+++ ui/moviestrip.reel/overlay.reel/overlay.load.js
@@ -1,1 +1,1 @@
-montageDefine("c12bfe6","ui/moviestrip.reel/overlay.reel/overlay",{dependencies:["montage/ui/component"],factory:function(e,n,o){var i=e("montage/ui/component").Component;n.Overlay=i.specialize({})}});
+montageDefine("e08f379","ui/moviestrip.reel/overlay.reel/overlay",{dependencies:["montage/ui/component"],factory:function(e,n,o){var i=e("montage/ui/component").Component;n.Overlay=i.specialize({})}});
