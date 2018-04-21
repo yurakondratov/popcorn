b7bbd4e00bf6c731bf1d08e90d64d861f72d3ebf
Yuri Kondratov <noreply>
2018-04-21 05:43:26 +0000

    Add component my-component-test

---
+++ ui/my-component-test.reel/my-component-test.js
@@ -0,0 +1,12 @@
+/**
+ * @module "ui/my-component-test.reel"
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class MyComponentTest
+ * @extends Component
+ */
+exports.MyComponentTest = Component.specialize(/** @lends MyComponentTest.prototype */{
+
+});
